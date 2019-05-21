56ce45ff5e06707ff9c44b4bef31c53a8fb38520
alekseygenerozov <aleksey.generozov@gmail.com>
2019-05-21 11:13:05 -0600

    More indexing issues.

--- Example_multimass_analysis.py
+++ Example_multimass_analysis.py
@@ -5,12 +5,12 @@ import numpy as np
 
 
 agrid=np.logspace(-3, 0, 100)
-agrid=agrid[:54]
+# agrid=agrid[:54]
 times=np.empty_like(agrid)
 
 fig,ax=plt.subplots(figsize=(10,9))
 for ii,aa in enumerate(agrid):
-	dat=np.genfromtxt('djj_multimass_{0}'.format(ii))
+	dat=np.genfromtxt('bk/djj_multimass_{0}'.format(ii))
 
 	times[ii]=IUS(dat[:,0], 2*dat[:,0]*dat[:,1]).integral(dat[0,0],dat[-1,0])**-1.0
 np.savetxt('t_rr_dat_multimass.csv', np.transpose([agrid, times]))
