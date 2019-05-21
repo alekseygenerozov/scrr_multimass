a30edb93d89bda56656ea3e0a8662cb6c7f0c253
alekseygenerozov <aleksey.generozov@gmail.com>
2019-05-21 11:13:53 -0600

    More indexing issues.

--- Example_multimass.py
+++ Example_multimass.py
@@ -9,7 +9,7 @@ idx=int(sys.argv[1])
 drr = DRR_Multimass(agrid[idx], gamma=[1.75, 2.0], mbh_mass=4.3e6, mass_frac=[1, 0.01], rh=2.0, star_mass=[1, 10], j_grid_size=64)
 djj, djj_err = drr(l_max=5)
 
-np.savetxt('djj_multimass_{0}'.format(ii), np.transpose([drr.j, 1.0e6*djj]))
+np.savetxt('djj_multimass_{0}'.format(idx), np.transpose([drr.j, 1.0e6*djj]))
 
 
 # f.close()
