1780008046e460a31e170e4c74dc6a840f7f657c
alekseygenerozov <aleksey.generozov@gmail.com>
2019-05-20 18:35:59 -0600

    Getting rid of loop in Example_multimass -- for use on cluster.

--- Example_multimass.py
+++ Example_multimass.py
@@ -5,13 +5,14 @@ import numpy as np
 
 
 agrid=np.logspace(-3, 0, 100)
-np.savetxt('a_multimass', agrid)
+# np.savetxt('a_multimass', agrid)
 # f=open('t_rr_dat_ehi.csv','w')
-for ii,aa in enumerate(agrid):
-	drr = DRR_Multimass(aa, gamma=[1.75, 2.0], mbh_mass=4.3e6, mass_frac=[1, 0.01], rh=2.0, star_mass=[1, 10], j_grid_size=64)
-	djj, djj_err = drr(l_max=5)
+# for ii,aa in enumerate(agrid):
+idx=agrid[sys.argv[1]]
+drr = DRR_Multimass(agrid[idx], gamma=[1.75, 2.0], mbh_mass=4.3e6, mass_frac=[1, 0.01], rh=2.0, star_mass=[1, 10], j_grid_size=64)
+djj, djj_err = drr(l_max=5)
 
-	np.savetxt('djj_multimass_{0}'.format(ii), np.transpose([drr.j, 1.0e6*djj]))
+np.savetxt('djj_multimass_{0}'.format(ii), np.transpose([drr.j, 1.0e6*djj]))
 
 
 # f.close()
