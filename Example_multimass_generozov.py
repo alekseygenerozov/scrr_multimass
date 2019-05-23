083fd4778117d42d1d618cd759d4b0c7df221f18
alekseygenerozov <aleksey.generozov@gmail.com>
2019-05-22 18:18:22 -0600

    Adding model tailored to Generozov+2018

---
+++ Example_multimass_generozov.py
@@ -0,0 +1,21 @@
+from drr_multimass import DRR_Multimass
+import matplotlib.pyplot as plt
+from scipy.interpolate import InterpolatedUnivariateSpline as IUS
+import numpy as np
+import sys
+
+agrid=np.logspace(-3, 0, 100)
+agrid=[0.001, 0.01]
+# idx=int(sys.argv[1])
+for idx in range(len(agrid)):
+	drr = DRR_Multimass(agrid[idx], gamma=[1.5, 1.8], mbh_mass=4.0e6, mass_frac=[1, 0.36], rh=3.6, star_mass=[0.3, 10], j_grid_size=64)
+	djj, djj_err = drr(l_max=5, neval=1e4)
+
+	np.savetxt('djj_multimass_{0}'.format(idx), np.transpose([drr.j, 1.0e6*djj]))
+
+
+# f.close()
+
+
+
+
