49ec1ece538dcc9a60a67c0c163915d71240c549
alekseygenerozov <aleksey.generozov@gmail.com>
2019-05-23 18:38:19 -0600

    generozov-fid10

---
+++ Example_multimass_generozov10.py
@@ -0,0 +1,20 @@
+from drr_multimass import DRR_Multimass
+import matplotlib.pyplot as plt
+from scipy.interpolate import InterpolatedUnivariateSpline as IUS
+import numpy as np
+import sys
+
+agrid=np.logspace(-3, 0, 100)
+
+for idx in range(len(agrid)):
+	drr = DRR_Multimass(agrid[idx], gamma=[1.5, 1.8], mbh_mass=4.0e6, mass_frac=[1, 0.6], rh=3.2, star_mass=[0.3, 10], j_grid_size=64)
+	djj, djj_err = drr(l_max=5, neval=1e4)
+
+	np.savetxt('djj_multimass_{0}'.format(idx), np.transpose([drr.j, 1.0e6*djj]))
+
+
+# f.close()
+
+
+
+
