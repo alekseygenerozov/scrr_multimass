0d8735621b04ffbb2ca59e0e73d0eb4b0ce1a13c
alekseygenerozov <aleksey.generozov@gmail.com>
2019-05-23 18:41:58 -0600

    Oops

--- Example_multimass_generozov10.py
+++ Example_multimass_generozov10.py
@@ -7,7 +7,7 @@ import sys
 agrid=np.logspace(-3, 0, 100)
 idx=int(sys.argv[1])
 # for idx in range(len(agrid)):
-drr = DRR_Multimass(agrid[idx], gamma=[1.5, 1.8], mbh_mass=4.0e6, mass_frac=[1, 0.6], rh=3.2, star_mass=[0.3, 10], j_grid_size=64)
+drr = DRR_Multimass(agrid[idx], gamma=[1.5, 1.8], mbh_mass=4.0e6, mass_frac=[1, 0.6], rh=3.3, star_mass=[0.3, 10], j_grid_size=64)
 djj, djj_err = drr(l_max=5, neval=1e4)
 
 np.savetxt('djj_multimass_{0}'.format(idx), np.transpose([drr.j, 1.0e6*djj]))
