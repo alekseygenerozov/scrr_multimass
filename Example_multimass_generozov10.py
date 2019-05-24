b60236dcf88ff9f0b608179850e041522da4cca8
alekseygenerozov <aleksey.generozov@gmail.com>
2019-05-23 18:41:08 -0600

    Getting rid of loop in generozov10

--- Example_multimass_generozov10.py
+++ Example_multimass_generozov10.py
@@ -5,12 +5,12 @@ import numpy as np
 import sys
 
 agrid=np.logspace(-3, 0, 100)
+idx=int(sys.argv[1])
+# for idx in range(len(agrid)):
+drr = DRR_Multimass(agrid[idx], gamma=[1.5, 1.8], mbh_mass=4.0e6, mass_frac=[1, 0.6], rh=3.2, star_mass=[0.3, 10], j_grid_size=64)
+djj, djj_err = drr(l_max=5, neval=1e4)
 
-for idx in range(len(agrid)):
-	drr = DRR_Multimass(agrid[idx], gamma=[1.5, 1.8], mbh_mass=4.0e6, mass_frac=[1, 0.6], rh=3.2, star_mass=[0.3, 10], j_grid_size=64)
-	djj, djj_err = drr(l_max=5, neval=1e4)
-
-	np.savetxt('djj_multimass_{0}'.format(idx), np.transpose([drr.j, 1.0e6*djj]))
+np.savetxt('djj_multimass_{0}'.format(idx), np.transpose([drr.j, 1.0e6*djj]))
 
 
 # f.close()
